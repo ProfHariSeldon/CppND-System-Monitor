07cd8acd636d805f47f90ffa6f8aeaca0565ddd5
Thomas H. Lipscomb <nano2ba@gmail.com>
2020-05-28 19:13:12 +0000

    Commit 2: Draft 2

--- src/system.cpp
+++ src/system.cpp
@@ -13,26 +13,29 @@ using std::size_t;
 using std::string;
 using std::vector;
 
+// https://github.com/wissalsayhi/udacity-CppND---System-Monitor/blob/master/src/system.cpp
+
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
+// HELP do I have to do anything here?
 vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization() { LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses() { LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+int System::TotalProcesses() { LinuxParser::TotalProcesses(); }
 
 // TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+long int System::UpTime() { LinuxParser::UpTime(); }
