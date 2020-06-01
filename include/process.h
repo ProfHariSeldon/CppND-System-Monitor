93e5636bd06437f4d25d0a1300d23fe8ab40892c
Thomas H. Lipscomb <nano2ba@gmail.com>
2020-06-01 12:22:45 +0000

    Add Process(int pit) to process.h fails to compile

--- include/process.h
+++ include/process.h
@@ -16,6 +16,9 @@ class Process {
   long int UpTime();                       // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
 
+  // https://knowledge.udacity.com/questions/212681
+  Process(int pid) : pid_(pid){}
+
   // TODO: Declare any necessary private members
   // https://github.com/rmorejon65/CppND-System-Monitor/blob/master/include/process.h
  private:
